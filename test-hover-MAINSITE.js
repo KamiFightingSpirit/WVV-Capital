6c8f0e096de89aed21345a9938f6e2efd4af688e
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-11-28 11:39:11 -0800

    All hovers done

--- test-hover-MAINSITE.js
+++ test-hover-MAINSITE.js
@@ -12,14 +12,139 @@
     //End Danielle D'Agostaro
 
     //Jason Franklin
-    $('#block-76b0ced63f31cd9e2342').mouseover(function() {
-    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2881b9ee0f32b87a52a14/1606584347864/Jason_Art.jpg");
+ //    $('#block-76b0ced63f31cd9e2342').mouseover(function() {
+ //    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2881b9ee0f32b87a52a14/1606584347864/Jason_Art.jpg");
+ //    });
+	// $('#block-76b0ced63f31cd9e2342').mouseout(function() {
+ //    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5e0a26c2afc7590ba0a3b443/1603914016365/Jason-5941.jpg");
+ //    })
+
+	$(function() {
+  		if ($('#bottom-art-jason').length) return; // only add once
+
+  		var jsonArt = '<img id="bottom-art-jason" src="https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2881b9ee0f32b87a52a14/1606584347864/Jason_Art.jpg" class="thumb-image" />';
+
+  	$('#block-76b0ced63f31cd9e2342').prepend(jsonArt);
+	});
+
+
+ 	//End Jason Franklin
+
+	/**
+		Leslie Lenzo, CFA
+		****************************************************
+		Block-ID:
+			block-c941ef6bbc69f7b10dbb
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2921be6d49a06bbe64c9c/1606586909839/Leslie_Art.jpg
+		Plain:
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569292311748-YXMYRM4ZHSIZ7Q3Z2ULI/ke17ZwdGBToddI8pDm48kA8xmmDawoWDb9mdCPDpNAJZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7Xj1nVWs2aaTtWBneO2WM-s1KGPCJ5SaFbPlL1J0tuDJZZbc2S-SlikIvH0AsWjk1A/Leslie+Lenzo+photo.jpg
+		****************************************************	
+	**/
+    $('#block-c941ef6bbc69f7b10dbb').mouseover(function() {
+    	$( "#block-c941ef6bbc69f7b10dbb .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2921be6d49a06bbe64c9c/1606586909839/Leslie_Art.jpg");
     });
-	$('#block-76b0ced63f31cd9e2342').mouseout(function() {
-    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5e0a26c2afc7590ba0a3b443/1603914016365/Jason-5941.jpg");
+	$('#block-c941ef6bbc69f7b10dbb').mouseout(function() {
+    	$( "#block-c941ef6bbc69f7b10dbb .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569292311748-YXMYRM4ZHSIZ7Q3Z2ULI/ke17ZwdGBToddI8pDm48kA8xmmDawoWDb9mdCPDpNAJZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7Xj1nVWs2aaTtWBneO2WM-s1KGPCJ5SaFbPlL1J0tuDJZZbc2S-SlikIvH0AsWjk1A/Leslie+Lenzo+photo.jpg");
     })
-  })
-  //End Jason Franklin
+	//End Leslie Lenzo, CFA
+
+	/**
+		Stacy Lewis
+		****************************************************
+		Block-ID:
+			block-yui_3_17_2_1_1604001558666_15429
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc29274cb3e0f5771a3b92d/1606586998308/Stacy_Lewis_DA2.jpg
+		Plain:
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1604012744726-8ISE0QZ9Y27V1IYH9BA5/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH33scGBZjC30S7EYewNF5iKKwhonf2ThqWWOBkLKnojuqYeU1KwPvsAK7Tx5ND4WE/7a0cec7f-b257-4ba2-8a51-dfea29a14e6d.jpg
+		****************************************************	
+	**/
+    $('#block-yui_3_17_2_1_1604001558666_15429').mouseover(function() {
+    	$( "#block-yui_3_17_2_1_1604001558666_15429 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc29274cb3e0f5771a3b92d/1606586998308/Stacy_Lewis_DA2.jpg");
+    });
+	$('#block-yui_3_17_2_1_1604001558666_15429').mouseout(function() {
+    	$( "#block-yui_3_17_2_1_1604001558666_15429 .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1604012744726-8ISE0QZ9Y27V1IYH9BA5/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH33scGBZjC30S7EYewNF5iKKwhonf2ThqWWOBkLKnojuqYeU1KwPvsAK7Tx5ND4WE/7a0cec7f-b257-4ba2-8a51-dfea29a14e6d.jpg");
+    })
+	//End Stacy Lewis
+
+	/**
+		Robert Locke
+		****************************************************
+		Block-ID:
+			block-436e86330fcd1f38f7f4
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc292534e98326c0264342e/1606586965500/Robert_Art.jpg	
+		Plain: 
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569283441997-UEP2GLZUZQQREN6G12VB/ke17ZwdGBToddI8pDm48kCdOlWObkxgrBeqSCv3YYUZZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxloOiFOzBWtWTqgrDcbTdtgImJbluRD5LI8d46kOd-z27PkYNHSgExLnaEX4BeIDQ/Robert+Locke+photo.jpg
+		****************************************************
+	**/
+  	$('#block-436e86330fcd1f38f7f4').mouseover(function() {
+    	$( "#block-436e86330fcd1f38f7f4 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc292534e98326c0264342e/1606586965500/Robert_Art.jpg");
+    });
+	$('#block-436e86330fcd1f38f7f4').mouseout(function() {
+    	$( "#block-436e86330fcd1f38f7f4 .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569283441997-UEP2GLZUZQQREN6G12VB/ke17ZwdGBToddI8pDm48kCdOlWObkxgrBeqSCv3YYUZZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxloOiFOzBWtWTqgrDcbTdtgImJbluRD5LI8d46kOd-z27PkYNHSgExLnaEX4BeIDQ/Robert+Locke+photo.jpg");
+    })
+	//End Robert Locke
+
+	/**
+		Matt Maurer
+		****************************************************
+		Block-ID:
+			block-yui_3_17_2_1_1603913892502_66234
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2923b1972c46e3cf1d09b/1606586940775/Matt_Art.jpg
+		Plain:
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1603914979336-TAPON6LVKPWQOGC7E7FM/ke17ZwdGBToddI8pDm48kL7-Fwch-HI7maaYwSVFCKRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIZIeP51CWkoM8MFvrwwi0zonCjl4lHiV9Cye-EQWhHs4/MM1+%281%29.png
+		****************************************************
+	**/
+  	$('#block-yui_3_17_2_1_1603913892502_66234').mouseover(function() {
+    	$( "#block-yui_3_17_2_1_1603913892502_66234 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2923b1972c46e3cf1d09b/1606586940775/Matt_Art.jpg");
+    });
+	$('#block-yui_3_17_2_1_1603913892502_66234').mouseout(function() {
+    	$( "#block-yui_3_17_2_1_1603913892502_66234 .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1603914979336-TAPON6LVKPWQOGC7E7FM/ke17ZwdGBToddI8pDm48kL7-Fwch-HI7maaYwSVFCKRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIZIeP51CWkoM8MFvrwwi0zonCjl4lHiV9Cye-EQWhHs4/MM1+%281%29.png");
+    })
+	//End Matt Maurer
+
+	/**
+		Steve Robinson Esq.
+		****************************************************
+		Block-ID:
+			block-8031bea02089f358316e
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc292a44e98326c02643e7c/1606587045738/Steve_Art.jpg
+		Plain:
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569875366966-N1Q7670A26ZMU8N3FJ8O/ke17ZwdGBToddI8pDm48kGDpvalPb1SqHoCn1hwN0Y57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQPoRzxSr1hzN-vPBHt7YyLLXgctAyUJRqJUUGWVDK_ZzIgvsybGcZEPqUYiXY8im/Steve+Robinson_web-square-0547.jpg
+		****************************************************
+	**/
+  	$('#block-8031bea02089f358316e').mouseover(function() {
+    	$( "#block-8031bea02089f358316e .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc292a44e98326c02643e7c/1606587045738/Steve_Art.jpg");
+    });
+	$('#block-8031bea02089f358316e').mouseout(function() {
+    	$( "#block-8031bea02089f358316e .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569875366966-N1Q7670A26ZMU8N3FJ8O/ke17ZwdGBToddI8pDm48kGDpvalPb1SqHoCn1hwN0Y57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQPoRzxSr1hzN-vPBHt7YyLLXgctAyUJRqJUUGWVDK_ZzIgvsybGcZEPqUYiXY8im/Steve+Robinson_web-square-0547.jpg");
+    })
+	//End Steve Robinson Esq.
+
+	/**
+		Craig Schedler
+		****************************************************
+		Block-ID:
+			block-df36591cc72d1fc5b195
+		Art:
+			https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc291eb2dd96f591841f84e/1606586861325/Craig_Art.jpg
+		Plain:
+			https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569292461571-8GNDE5CRNMUQHKWNSXST/ke17ZwdGBToddI8pDm48kCOUdsrpYefXDbjZq4MWA1F7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lqfkVpRp1g_2L-WsTQRP4L5QxBJedHzK2o27RMX5Q6UPpOK9Q3ljjx6VOw6lNn8cA/Craig.jpg
+		****************************************************
+	**/
+
+  	$('#block-df36591cc72d1fc5b195').mouseover(function() {
+    	$( "#block-df36591cc72d1fc5b195 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc291eb2dd96f591841f84e/1606586861325/Craig_Art.jpg");
+    });
+	$('#block-df36591cc72d1fc5b195').mouseout(function() {
+    	$( "#block-df36591cc72d1fc5b195 .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1569292461571-8GNDE5CRNMUQHKWNSXST/ke17ZwdGBToddI8pDm48kCOUdsrpYefXDbjZq4MWA1F7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lqfkVpRp1g_2L-WsTQRP4L5QxBJedHzK2o27RMX5Q6UPpOK9Q3ljjx6VOw6lNn8cA/Craig.jpg");
+    })
+	//End Craig Schedler
 
 })(jQuery);
 </script>
+
