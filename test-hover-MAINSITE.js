49b380a15d55cb7cf663d9f4097f50349a2c7f47
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-11-28 10:03:51 -0800

    Two Members Working on Main Site

---
+++ test-hover-MAINSITE.js
@@ -0,0 +1,25 @@
+<script>
+(function($) {
+  $(document).ready(function() {
+    //Danielle D'Agostaro
+    $('#block-yui_3_17_2_1_1603915738299_61644').mouseover(function() {
+    	$( "#block-yui_3_17_2_1_1603915738299_61644 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc29057cb3e0f5771a3629b/1606586457570/Danielle_Art.jpg");
+    });
+	$('#block-yui_3_17_2_1_1603915738299_61644').mouseout(function() {
+    	$( "#block-yui_3_17_2_1_1603915738299_61644 .thumb-image" ).attr("src","https://images.squarespace-cdn.com/content/v1/5cae826d21d24e00012dc02b/1603917510862-L39EP3KNT8M9RDH8A94E/ke17ZwdGBToddI8pDm48kKaTVuD7YHb7wRPju8oqY6J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UThCi8D0XusHafWejgc2tI1BtSBVunXyFfqDTnmmWWDml4I0KDeTo5c4Onxt7bt-VQ/RG0A0265_.jpg");
+    })
+
+    //End Danielle D'Agostaro
+
+    //Jason Franklin
+    $('#block-76b0ced63f31cd9e2342').mouseover(function() {
+    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5fc2881b9ee0f32b87a52a14/1606584347864/Jason_Art.jpg");
+    });
+	$('#block-76b0ced63f31cd9e2342').mouseout(function() {
+    	$( "#block-76b0ced63f31cd9e2342 .thumb-image" ).attr("src","https://static1.squarespace.com/static/5cae826d21d24e00012dc02b/t/5e0a26c2afc7590ba0a3b443/1603914016365/Jason-5941.jpg");
+    })
+  })
+  //End Jason Franklin
+
+})(jQuery);
+</script>
